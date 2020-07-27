45ac5a1971afbb8b67cd0c7395a54b3a6f6cd7ec
dipock das <dipockdas@gmail.com>
2020-07-27 15:38:44 -0700

    Completed app
    Simple app with dockerfile to package app and keep environment separate from local mc.

---
+++ app/src/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import os
+server = Flask(__name__)
+
+@server.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+   port = int(os.environ.get("PORT", 5000))
+   server.run(debug=True,host='0.0.0.0',port=port)
